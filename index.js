5b657600385ac6320f8780e05d8bf89cc39b686c
Paulo Ricardo <paulo.silva56@estudante.ifto.edu.br>
2026-06-02 19:37:39 -0300

    Adição de suporte a URL Encoded
    Além da possibilidade receber o JSON, a API também poderá receber informações em formato de formulário.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000 
 
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 const router = require("./routes/moduleRoute.js")
 const criarConexao = require("./connection/connection.js")
